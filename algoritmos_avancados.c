f48244994d52b8e7c36e2050d0d52d9bc44fc254
Ana <anacris0623@gmail.com>
2025-10-19 05:41:25 +0000

    detective quest desafio nivel novato

--- algoritmos_avancados.c
+++ algoritmos_avancados.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
-
 // Desafio Detective Quest
 // Tema 4 - Árvores e Tabela Hash
 // Este código inicial serve como base para o desenvolvimento das estruturas de navegação, pistas e suspeitos.
 // Use as instruções de cada região para desenvolver o sistema completo com árvore binária, árvore de busca e tabela hash.
-
-int main() {
 
     // 🌱 Nível Novato: Mapa da Mansão com Árvore Binária
     //
@@ -42,6 +38,149 @@ int main() {
     // - Em caso de colisão, use lista encadeada para tratar.
     // - Modularize com funções como inicializarHash(), buscarSuspeito(), listarAssociacoes().
 
+//--CODIGO
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SALAS 20 //--numero maximo de salas visitadas
+
+//--definindo a estrutura e criação do nó (sala)
+
+/* Começamos com a definição da sala. Ela conterá uma string nome (com até 49 caracteres + \0)
+e dois ponteiros: um para a sala à esquerda e outro para o que está à direita.*/
+
+typedef struct Sala {
+    char nomeSala[50];
+    struct Sala *esquerda;
+    struct Sala *direita;
+} Sala;
+
+//---limpar buffer de entrada para evitar problema de leitura consecutiva de scanf e getchar
+void limparBuffer(){
+    int buffer;
+    while ((buffer = getchar()) != '\n' && buffer != EOF);
+}
+
+/* Neste desafio, vamos definir, de modo manual, nossa árvore (mansão). Então, não faremos a função inserir.
+Desse modo, basta criamos uma função para alocar um novo nó (sala) na memória:*/
+
+Sala *criarSala(char* nomeSala) {
+    Sala *novaSala = (Sala*)malloc(sizeof(Sala));
+
+    //--confere se a alocacao funcionou; se nao, retorna 1 para indicar erro
+    if (novaSala == NULL){
+        printf("Erro: Falha ao alocar memoria.\n");
+    }
+
+    strcpy(novaSala->nomeSala, nomeSala);
+    novaSala->esquerda = NULL;
+    novaSala->direita = NULL;
+    return novaSala;
+}
+
+/* Criação da função para explorar a mansão a partir de uma sala.*/
+void explorarSala(Sala *atual, char *salasVisitadas[], int *contador){
+    
+    //--aloca memoria para armazenar o nome da sala visitada atualmente
+    //(caso o limite maximo nao tenha sido atingido)
+    if (*contador < MAX_SALAS) {
+        salasVisitadas[*contador] = malloc(strlen(atual->nomeSala) + 1);
+        strcpy(salasVisitadas[*contador], atual->nomeSala); //--copia o nome da sala para a memoria
+        (*contador)++;
+        //--viabilizar a exploração continua até o jogador alcançar um cômodo que não possua
+        //caminhos à esquerda nem à direita (isto é, um nó-folha na árvore).
+        if (atual->esquerda == NULL && atual->direita == NULL) {
+        printf("\nVoce chegou ao fim da exploracao (nó-folha).\n");
+        return;
+        }
+    }
+
+    printf("\nVoce esta em \"%s\".\n", atual->nomeSala); //--sala atual onde o jogador esta
+
+    char escolha;
+    printf("\nEscolha o caminho que deseja seguir:\n");
+    if (atual->esquerda != NULL){
+        printf("(e) Ir para a esquerda, \"%s\"\n", atual->esquerda->nomeSala);
+    }
+    if (atual->direita != NULL){
+        printf("(d) Ir para a direita, \"%s\"\n", atual->direita->nomeSala);
+    }
+    printf("(s) Sair\n");
+    printf("Opcao: ");
+    scanf("%c", &escolha);
+    limparBuffer(); //--limpa o buffer
+
+    //--direcoes onde o jogador ira a partir da escolha feita
+    if (escolha == 'e' && atual->esquerda != NULL) {
+        explorarSala(atual->esquerda, salasVisitadas, contador);
+    } else if (escolha == 'd' && atual->direita != NULL) {
+        explorarSala(atual->direita, salasVisitadas, contador);
+    } else if (escolha == 's') {
+        printf("Saindo...\n");
+        return;
+    } else {
+        printf("Opcao invalida. Tente novamente.\n");
+        explorarSala(atual, salasVisitadas, contador);
+    }
+}
+
+//--é preciso liberar a memória ocupada pela árvore após o uso, evitando vazamentos:
+void liberarMansao(struct Sala* raiz){
+    if (raiz != NULL){
+        liberarMansao(raiz->esquerda);
+        liberarMansao(raiz->direita);
+        free(raiz);
+    }
+}
+
+int main(){
+    //--criando as salas manualmente
+    Sala *hall = criarSala("Hall de Entrada");
+    Sala *salaEstar = criarSala("Sala de Estar");
+    Sala *cozinha = criarSala("Cozinha");
+    Sala *biblioteca = criarSala("Biblioteca");
+    Sala *lavanderia = criarSala("Lavanderia");
+    Sala *quartoC = criarSala("Quarto Casal");
+    Sala *quartoS = criarSala("Quarto Solteiro");
+    Sala *banheiro = criarSala("Banheiro");
+
+    //--ramificacao da mansao
+    hall->esquerda = salaEstar;
+    hall->direita = cozinha;
+
+    salaEstar->esquerda = biblioteca;
+    salaEstar->direita = quartoC;
+
+    biblioteca->esquerda = lavanderia;
+
+    cozinha->esquerda = quartoS;
+    cozinha->direita = banheiro;
+
+    //--vetor para armazenar o caminho percorrido
+    char *salasVisitadas[MAX_SALAS];
+    int contador = 0;
+
+    //--exploracao usando recursividade
+    printf("=================================================\n");
+    printf("             Inicio da exploracao!\n");
+    printf("=================================================\n");
+    explorarSala(hall, salasVisitadas, &contador);
+
+    //--exibir caminho percorrido
+    printf("=================================================\n");
+    printf("              Caminho percorrido\n");
+    printf("=================================================\n");
+    for (int i = 0; i < contador; i++) {
+        printf("=> %s\n", salasVisitadas[i]);
+        free(salasVisitadas[i]);  //--libera memoria de cada nome
+    }
+
+    //-liberacao da memoria das salas
+    liberarMansao(hall);
+
     return 0;
 }
+
+
 
