3b6a84f0f3c1896a8a1246b19068eb6d0e6d3073
Ana <anacris0623@gmail.com>
2025-10-19 05:46:33 +0000

    detective quest desafio aventureiro

--- algoritmos_avancados.c
+++ algoritmos_avancados.c
@@ -45,16 +45,24 @@
 
 #define MAX_SALAS 20 //--numero maximo de salas visitadas
 
-//--definindo a estrutura e criação do nó (sala)
+//--definindo a estrutura e criação dos nós (sala) e (pista)
 
-/* Começamos com a definição da sala. Ela conterá uma string nome (com até 49 caracteres + \0)
-e dois ponteiros: um para a sala à esquerda e outro para o que está à direita.*/
-
+/*Começamos com a definição da sala. Ela conterá uma string nome (com até 49 caracteres + \0)
+e dois ponteiros: um para a sala à esquerda e outro para o que está à direita. Além disso,
+conterá uma string pista associada a sala.*/
 typedef struct Sala {
     char nomeSala[50];
+    char pista[100];
     struct Sala *esquerda;
     struct Sala *direita;
 } Sala;
+
+//Em seguida, criamos o nó para as pistas.
+typedef struct Pista{
+    char pista[100];
+    struct Pista *esquerda;
+    struct Pista *direita;
+} Pista;
 
 //---limpar buffer de entrada para evitar problema de leitura consecutiva de scanf e getchar
 void limparBuffer(){
@@ -62,25 +70,82 @@ void limparBuffer(){
     while ((buffer = getchar()) != '\n' && buffer != EOF);
 }
 
-/* Neste desafio, vamos definir, de modo manual, nossa árvore (mansão). Então, não faremos a função inserir.
-Desse modo, basta criamos uma função para alocar um novo nó (sala) na memória:*/
+/*Vamos definir, de modo manual, nossa árvore (mansão). Então, não faremos a função inserir.
+Desse modo, basta criamos uma função para alocar um novo nó (sala) na memória, criando dinamicamente uma
+sala com nome e pista (opcional)*/
 
-Sala *criarSala(char* nomeSala) {
+Sala *criarSala(char* nomeSala, char *pista) {
     Sala *novaSala = (Sala*)malloc(sizeof(Sala));
 
-    //--confere se a alocacao funcionou; se nao, retorna 1 para indicar erro
+    //--confere se a alocacao funcionou
     if (novaSala == NULL){
         printf("Erro: Falha ao alocar memoria.\n");
     }
 
     strcpy(novaSala->nomeSala, nomeSala);
+    strcpy(novaSala->pista, pista);
+
     novaSala->esquerda = NULL;
     novaSala->direita = NULL;
     return novaSala;
 }
 
+/*Criando um nó para as pistas
+Sempre que quisermos adicionar um novo valor à árvore, precisaremos criar um nó com esse valor.
+Essa função aloca memória e inicializa os ponteiros como NULL, já que novos nós começam sem filhos.*/
+Pista *criarPista(char* pista) {
+    Pista *novaPista = (Pista*)malloc(sizeof(Pista));
+
+    //--confere se a alocacao funcionou
+    if (novaPista == NULL){
+        printf("Erro: Falha ao alocar memoria.\n");
+    }
+    strcpy(novaPista->pista, pista);
+    novaPista->esquerda = NULL;
+    novaPista->direita = NULL;
+    return novaPista;
+}
+
+/*Inserindo elementos na árvore pista
+O passo agora é inserir os nós seguindo a regra da BST: valores menores vão à esquerda, maiores à direita:*/
+Pista *inserirPista(Pista *raiz, char *pista) {
+    if (raiz == NULL){ 
+        return criarPista(pista);
+    }
+    
+    if (strcmp(pista, raiz->pista) < 0){
+        raiz->esquerda = inserirPista(raiz->esquerda, pista);
+    } else if (strcmp(pista, raiz->pista) > 0){
+        raiz->direita = inserirPista(raiz->direita, pista);
+    }
+    
+    return raiz;
+}
+
+//Exibindo pistas em ordem alfabetica (in-order)
+/*A principal vantagem da BST é poder ser percorrida em ordem crescente com um algoritmo simples.
+Esse percurso visita primeiro a esquerda, depois o nó atual, e, por fim, a direita.*/
+void exibirPistas(Pista *raiz) {
+    if (raiz != NULL) {
+        exibirPistas(raiz->esquerda);
+        printf("\"%s\"\n", raiz->pista);
+        exibirPistas(raiz->direita);
+    }
+}
+
+//Liberando a arvore de pistas
+void liberarPistas(Pista *raiz) {
+    if (raiz != NULL) {
+        liberarPistas(raiz->esquerda);
+        liberarPistas(raiz->direita);
+        free(raiz);
+    }
+}
+//-----Atenção
+//Esse passo certifica que cada valor inserido é um novo bloco na árvore, isolado e pronto para ser conectado a outros nós.
+
 /* Criação da função para explorar a mansão a partir de uma sala.*/
-void explorarSala(Sala *atual, char *salasVisitadas[], int *contador){
+void explorarSala(Sala *atual, char *salasVisitadas[], int *contador, Pista **raizPistas){
     
     //--aloca memoria para armazenar o nome da sala visitada atualmente
     //(caso o limite maximo nao tenha sido atingido)
@@ -90,13 +155,22 @@ void explorarSala(Sala *atual, char *salasVisitadas[], int *contador){
         (*contador)++;
         //--viabilizar a exploração continua até o jogador alcançar um cômodo que não possua
         //caminhos à esquerda nem à direita (isto é, um nó-folha na árvore).
-        if (atual->esquerda == NULL && atual->direita == NULL) {
-        printf("\nVoce chegou ao fim da exploracao (nó-folha).\n");
-        return;
-        }
     }
 
     printf("\nVoce esta em \"%s\".\n", atual->nomeSala); //--sala atual onde o jogador esta
+
+    //--adicionando pistas na sala
+    if (strlen(atual->pista) > 0) {
+        printf("Pista: \"%s\"\n", atual->pista);
+        *raizPistas = inserirPista(*raizPistas, atual->pista);
+    } else {
+        printf("Nenhuma pista encontrada aqui.\n");
+    }
+
+    if (atual->esquerda == NULL && atual->direita == NULL) {
+        printf("\nVoce chegou ao fim da exploracao.\n");
+        return;
+    }
 
     char escolha;
     printf("\nEscolha o caminho que deseja seguir:\n");
@@ -113,15 +187,15 @@ void explorarSala(Sala *atual, char *salasVisitadas[], int *contador){
 
     //--direcoes onde o jogador ira a partir da escolha feita
     if (escolha == 'e' && atual->esquerda != NULL) {
-        explorarSala(atual->esquerda, salasVisitadas, contador);
+        explorarSala(atual->esquerda, salasVisitadas, contador, raizPistas);
     } else if (escolha == 'd' && atual->direita != NULL) {
-        explorarSala(atual->direita, salasVisitadas, contador);
+        explorarSala(atual->direita, salasVisitadas, contador, raizPistas);
     } else if (escolha == 's') {
         printf("Saindo...\n");
         return;
     } else {
         printf("Opcao invalida. Tente novamente.\n");
-        explorarSala(atual, salasVisitadas, contador);
+        explorarSala(atual, salasVisitadas, contador, raizPistas);
     }
 }
 
@@ -136,14 +210,14 @@ void liberarMansao(struct Sala* raiz){
 
 int main(){
     //--criando as salas manualmente
-    Sala *hall = criarSala("Hall de Entrada");
-    Sala *salaEstar = criarSala("Sala de Estar");
-    Sala *cozinha = criarSala("Cozinha");
-    Sala *biblioteca = criarSala("Biblioteca");
-    Sala *lavanderia = criarSala("Lavanderia");
-    Sala *quartoC = criarSala("Quarto Casal");
-    Sala *quartoS = criarSala("Quarto Solteiro");
-    Sala *banheiro = criarSala("Banheiro");
+    Sala *hall = criarSala("Hall de Entrada", "Pegadas que levam a Cozinha");
+    Sala *salaEstar = criarSala("Sala de Estar", "Vaso quebrado");
+    Sala *cozinha = criarSala("Cozinha", "Faca faltando no faqueiro");
+    Sala *biblioteca = criarSala("Biblioteca", "Livro de Quimica faltando da estante");
+    Sala *lavanderia = criarSala("Lavanderia", "Sem pistas");
+    Sala *quartoC = criarSala("Quarto Casal", "Sem pistas");
+    Sala *quartoS = criarSala("Quarto Solteiro", "Manchas vermelhas no carpete");
+    Sala *banheiro = criarSala("Banheiro", "Parece que foi limpo recentemente");
 
     //--ramificacao da mansao
     hall->esquerda = salaEstar;
@@ -161,11 +235,14 @@ int main(){
     char *salasVisitadas[MAX_SALAS];
     int contador = 0;
 
+    //--pistas
+    Pista *arvorePistas = NULL;
+
     //--exploracao usando recursividade
     printf("=================================================\n");
     printf("             Inicio da exploracao!\n");
     printf("=================================================\n");
-    explorarSala(hall, salasVisitadas, &contador);
+    explorarSala(hall, salasVisitadas, &contador, &arvorePistas);
 
     //--exibir caminho percorrido
     printf("=================================================\n");
@@ -176,11 +253,17 @@ int main(){
         free(salasVisitadas[i]);  //--libera memoria de cada nome
     }
 
+    //--exibir pistas em ordem alfabetica
+    printf("=================================================\n");
+    printf("     Pistas encontradas (em ordem alfabetica)\n");
+    printf("=================================================\n");
+
+    exibirPistas(arvorePistas);
+
     //-liberacao da memoria das salas
     liberarMansao(hall);
+    liberarPistas(arvorePistas);
 
     return 0;
 }
-
-
 
